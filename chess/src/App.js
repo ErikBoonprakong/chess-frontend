72e8a2dd7a4f399b4b027cb5522ece4f97fceccf
ErikBoonprakong <e.boonprakong@gmail.com>
2022-02-21 12:26:53 +0000

    Added homepage with redirect buttons

--- chess/src/App.js
+++ chess/src/App.js
@@ -3,12 +3,19 @@ import "./App.css";
 import CreateAccount from "./Components/CreateAccount";
 import LogIn from "./Components/LogIn";
 import { Switch, Route, Redirect, Link } from "react-router-dom";
+import HomePage from "./Components/HomePage";
+import PlayAI from "./Components/PlayAI";
+import PlayOnline from "./Components/PlayOnline";
+import Leaderboards from "./Components/PlayOnline copy";
 
 function App() {
   return (
     <div className="App">
       <Link to="/register">
         <button>Create Account</button>
+      </Link>
+      <Link to="/home">
+        <button>Test home page</button>
       </Link>
       <Switch>
         <Route exact path="/">
@@ -19,6 +26,18 @@ function App() {
         </Route>
         <Route path="/register">
           <CreateAccount />
+        </Route>
+        <Route path="/home">
+          <HomePage />
+        </Route>
+        <Route path="/playai">
+          <PlayAI />
+        </Route>
+        <Route path="/playonline">
+          <PlayOnline />
+        </Route>
+        <Route path="/leaderboards">
+          <Leaderboards />
         </Route>
       </Switch>
     </div>
---
+++ chess/src/Components/PlayOnline copy.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Networking from "./Networking.js";
+import { Link, Redirect } from "react-router-dom";
+import "./home.css";
+
+class Leaderboards extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="bodyWrapperHome">
+        <h1>Leaderboards</h1>
+      </div>
+    );
+  }
+}
+
+export default Leaderboards;
