abeeb19c9c6172b996e8391c08a8f484ea4b66dc
ErikBoonprakong <e.boonprakong@gmail.com>
2022-02-21 17:49:36 +0000

    updated to run with deployed backend

--- chess/src/Components/Networking.js
+++ chess/src/Components/Networking.js
@@ -1,4 +1,6 @@
-const API_URL = "http://localhost:8080";
+// const API_URL = process.env.REACT_APP_API_URL;
+// const API_URL = "http://localhost:8080";
+const API_URL = "https://chessyem.herokuapp.com";
 
 class Networking {
   async postUser(username, password, confirmation) {
