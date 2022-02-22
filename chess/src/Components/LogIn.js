58f06a52d58f2c66699818444a6c1309066f5803
Yassin Serroukh <yassinserroukh00@gmail.com>
2022-02-22 15:20:17 +0000

    Changed credentials to true

--- chess/src/Components/LogIn.js
+++ chess/src/Components/LogIn.js
@@ -30,7 +30,7 @@ class LogIn extends React.Component {
     );
 
     this.setState({ cookies: cookieObj() });
-    console.log(this.state);
+    console.log(this.state.cookies);
     this.props.newCookie(this.state.cookies);
     if (this.state.cookies.user) {
       this.setState({ redirect: true, valid: true });
