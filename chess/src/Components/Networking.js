a7d06a4c200d4b3ceece12a60d900719c369c25a
ErikBoonprakong <e.boonprakong@gmail.com>
2022-02-28 18:46:30 +0000

    fixed leaderboard rendering issues

--- chess/src/Components/Networking.js
+++ chess/src/Components/Networking.js
@@ -105,7 +105,7 @@ class Networking {
   }
 
   async getScores() {
-    const response = await fetch(`${API_URL}/scores`);
+    const response = await fetch(`${API_URL}/saves`);
     let scores = await response.json();
     console.log(scores);
     return scores;
