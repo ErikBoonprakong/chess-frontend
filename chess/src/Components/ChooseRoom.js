5fec8c709a0c86ecf94dcd1834fd5c1dc11b5436
Megan Watson <meganwatson@Megans-MacBook-Pro.local>
2022-03-03 21:58:25 +0000

    took out console logs and added some comments

--- chess/src/Components/ChooseRoom.js
+++ chess/src/Components/ChooseRoom.js
@@ -3,10 +3,6 @@ import { Link, Redirect } from "react-router-dom";
 import io from "socket.io-client";
 
 class ChooseRoom extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div>
