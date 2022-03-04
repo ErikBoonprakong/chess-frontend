25883fba4a57d8c276f7d070d4016e7fcf040609
ErikBoonprakong <e.boonprakong@gmail.com>
2022-03-04 09:25:51 +0000

    added more rooms

--- chess/src/Components/ChooseRoom.js
+++ chess/src/Components/ChooseRoom.js
@@ -20,7 +20,7 @@ class ChooseRoom extends React.Component {
             },
           }}
         >
-          <button id="bathroom" value="bathroom">
+          <button id="bathroom" value="bathroom" className="room-btn">
             Bathroom
           </button>
         </Link>
@@ -30,7 +30,7 @@ class ChooseRoom extends React.Component {
             state: { roomNumber: 1, roomName: "livingRoom" },
           }}
         >
-          <button id="livingRoom" value="livingRoom">
+          <button id="livingRoom" value="livingRoom" className="room-btn">
             Living Room
           </button>
         </Link>
@@ -40,7 +40,7 @@ class ChooseRoom extends React.Component {
             state: { roomNumber: 2, roomName: "diningRoom" },
           }}
         >
-          <button id="diningRoom" value="diningRoom">
+          <button id="diningRoom" value="diningRoom" className="room-btn">
             Dining Room
           </button>
         </Link>
@@ -50,8 +50,68 @@ class ChooseRoom extends React.Component {
             state: { roomNumber: 3, roomName: "basement" },
           }}
         >
-          <button id="basement" value="basement">
-            Dining Room
+          <button id="basement" value="basement" className="room-btn">
+            Basement
+          </button>
+        </Link>
+        <Link
+          to={{
+            pathname: "/playonline",
+            state: { roomNumber: 4, roomName: "yassinsRoom" },
+          }}
+        >
+          <button id="yassinsRoom" value="yassinsRoom" className="room-btn">
+            Yassin's Room
+          </button>
+        </Link>
+        <Link
+          to={{
+            pathname: "/playonline",
+            state: { roomNumber: 5, roomName: "megansRoom" },
+          }}
+        >
+          <button id="megansRoom" value="megansRoom" className="room-btn">
+            Megan's Room
+          </button>
+        </Link>
+        <Link
+          to={{
+            pathname: "/playonline",
+            state: { roomNumber: 6, roomName: "eriksRoom" },
+          }}
+        >
+          <button id="eriksRoom" value="eriksRoom" className="room-btn">
+            Erik's Room
+          </button>
+        </Link>
+        <Link
+          to={{
+            pathname: "/playonline",
+            state: { roomNumber: 7, roomName: "kitchen" },
+          }}
+        >
+          <button id="kitchen" value="kitchen" className="room-btn">
+            Kitchen
+          </button>
+        </Link>
+        <Link
+          to={{
+            pathname: "/playonline",
+            state: { roomNumber: 8, roomName: "loft" },
+          }}
+        >
+          <button id="loft" value="loft" className="room-btn">
+            Loft
+          </button>
+        </Link>
+        <Link
+          to={{
+            pathname: "/playonline",
+            state: { roomNumber: 9, roomName: "garden" },
+          }}
+        >
+          <button id="garden" value="garden" className="room-btn">
+            Garden
           </button>
         </Link>
       </div>
